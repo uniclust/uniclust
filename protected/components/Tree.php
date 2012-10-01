de27f6bc805cf6f7578b773cecbf34773d4d5c79
Alexey Salnikov <alexey.salnikov@gmail.com>
2012-10-01 12:52:42 +0400

    Remove all for migrating aligner into this place

--- protected/components/Tree.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Tree extends CWidget {
-    public function run() {
-        $model = Interests::model()->findByPK(1); // Здесь вместо Categories меняем на свою модель
-        $tree = $model->getTreeViewData(false);
-        $this->render('tree',array('tree'=>$tree,));
-    }
-}
--- protected/components/views/dobPicker.php
+++
@@ -1,5 +0,0 @@
-<div id="dobPicker">
-	<?php echo CHtml::dropDownList($this->dayField, $this->day, $this->getDays(), $this->getDayHtmlOptions()); ?>
-	<?php echo CHtml::dropDownList($this->monthField, $this->month, $this->getMonths(), $this->getMonthHtmlOptions()); ?>
-	<?php echo CHtml::dropDownList($this->yearField, $this->year, $this->getYears(), $this->getYearHtmlOptions()); ?>
-</div>
