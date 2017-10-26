2e096acdac4f4fb0bf26ad8be89ee94266b26924
Dmitriy <revk_a@rambler.ru>
2017-10-26 23:25:37 +0300

    Update abstract_db.py

--- ssh_run/uniclust/abstract_db.py
+++ ssh_run/uniclust/abstract_db.py
@@ -1,13 +1,32 @@
 # -*- coding: utf-8 -*
+try:
+    import pymysql as sql
+    sql.install_as_MySQLdb()
+except ImportError:
+    pass
 
 class Db_connection(object):
-
-    def    __init__(self,config):
+    def    __init__(self, *args, **kwargs):
         """
         Creating database object and connecting it to
         really database
         """
         self.db = None
+        self.error = False
+        self.error_message = '';
+        
+        try:
+            self.db = sql.connect(*args, **kwargs);
+        except sql.InternalError as str:
+            print(str.args);
+            
+        self.curs = self.db.cursor();
+        
+    def get_cursor(self):
+        return self.db.cursor();
+        return False;
+    def execute_query( self, query ):
+        return self.curs.execute( query );
 
-
-
+    def fetchall(self):
+        return self.curs.fetchall();
