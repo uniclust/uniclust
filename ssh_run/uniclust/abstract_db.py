d6d26f69fc1a579c67119c1a1cadb6ab8fb4575a
Dmitriy <revk_a@rambler.ru>
2017-10-31 21:08:27 +0300

    Update abstract_db.py

--- ssh_run/uniclust/abstract_db.py
+++ ssh_run/uniclust/abstract_db.py
@@ -29,14 +29,7 @@ class Db_connection(object):
         """
         Select all operations that has status 'new' and return class operations
         """
-        query=\
-        """
-        select 
-            *
-        from
-            operations
-
-        """
+        query= "SELECT * FROM `operations` ORDER BY `operation_id` ";
         if self.debug:
             print(query);
 
@@ -57,16 +50,8 @@ class Db_connection(object):
         Return `cl_file` python class
         """
 
-        query =\
-        """
-        SELECT 
-            *
-        FROM 
-            `files`
-        WHERE 
-            `file_id`='%d' LIMIT 1
-        
-        """ % file_id;
+        query = "SELECT * FROM `files` WHERE `file_id`='%d' LIMIT 1" % file_id;
+
         if self.debug:
             print(query);
 
@@ -77,3 +62,33 @@ class Db_connection(object):
             return class_files.cl_file(result[0]);
 
         return False;
+    
+    def query_lock_operation(self, **kwargs):
+        """
+        Lock/Unlock операцию. Принимает минимум 1 аргумент ввиде 
+        cl_oper=class_operation 
+        error=string
+        Use query_lock_operation(cl_oper=this) чтобы lock операцию и файл который используется в ней
+        Use query_lock_operation(cl_oper=this, error="") чтобы unlock операцию без ошибки;
+        Use query_lock_operation(cl_oper=this, error="Error") чтобы разблокировать операцию с ошибкой;
+        """
+
+        RUNNING_STATUS = 2;
+
+        if len(kwargs) < 1 or 'cl_oper' not in kwargs.keys():
+            return -1; # Error
+
+        if len(kwargs) == 1:
+            arg = kwargs['cl_oper'];
+            query = "UPDATE `operations` SET `status`=%d WHERE `operation_id`='%d'; UPDATE `files` set `status` = 2 WHERE file_id = %d"%(RUNNING_STATUS,arg.oper_id, arg.file_id);
+            self.execute_query(query);
+
+            if self.debug:
+                print (query);
+
+            return;
+
+        arg2 = kwargs['error'];
+        query = "UPDATE `operations` SET `status`=%d, `error_message`='%s' WHERE operation_id`='%d';\
+                UPDATE `files` SET `status`='ready' WHERE `file_id`=%d" % ( 3 if not len(arg2) else 5, arg2, arg.oper_id, arg.file_id);
+
