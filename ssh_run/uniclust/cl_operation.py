1c5d7b677cebc9cd9262a2e753a3c0311eea768e
Dmitriy <revk_a@rambler.ru>
2017-11-02 23:31:45 +0300

    Delete cl_operation.py

--- ssh_run/uniclust/cl_operation.py
+++
@@ -1,9 +0,0 @@
-class cl_operation(object):
-    """description of class"""
-    def __init__(self,  oper_object ):
-        self.oper_id =              oper_object[0];
-        self.file_id =              oper_object[1];
-        self.oper_type =            oper_object[2];
-        self.multiprocessor_id =    oper_object[3];
-        self.status =               oper_object[4];
-        self.error_message =        oper_object[5];
