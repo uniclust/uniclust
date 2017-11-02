91832d758935533c48c68e8f4ad38a389ef6bae8
Dmitriy <revk_a@rambler.ru>
2017-11-02 23:31:36 +0300

    Delete cl_file.py

--- ssh_run/uniclust/cl_file.py
+++
@@ -1,8 +0,0 @@
-class cl_file(object):
-    """description of class"""
-    def __init__(self,  file_object ):
-        self.file_id =      file_object[0];
-        self.name =         file_object[1];
-        self.status =       file_object[2];
-        self.user_id =      file_object[3];
-        self.size =         file_object[4];
